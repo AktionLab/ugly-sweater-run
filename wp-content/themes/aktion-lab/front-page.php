0b66877493a02a1f5b6def49d857d5a50b6d440f
KevHotaling <khotaling@gmail.com>
2012-09-04 00:56:14 -0600

    Home Page Styling

---
+++ wp-content/themes/aktion-lab/front-page.php
@@ -0,0 +1,20 @@
+<div id="hero" class="row">
+  <?php the_content(); ?>
+  <nav id="nav-cities" role="navigation">
+    <?php wp_nav_menu(array('theme_location' => 'cities_navigation', 'menu_class' => 'nav')); ?>
+  </nav>
+</div>
+<div id="home-boxes" class="row">
+  <div class="hb-about span3">
+    <h2><a href="">What is it?</a></h2>
+  </div>
+  <div class="hb-find span3">
+    <h2><a href="">Find a run</a></h2>
+  </div>
+  <div class="hb-gallery span3">
+    <h2><a href="">Gallery</a></h2>
+  </div>
+  <div class="hb-press span3">
+    <h2><a href="">Press</a></h2>
+  </div>
+</div><!--row-->  
