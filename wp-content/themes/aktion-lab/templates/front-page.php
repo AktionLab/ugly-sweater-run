d9b55c744556499b540b75d421bc5411d2fd7599
KevHotaling <khotaling@gmail.com>
2012-09-03 21:51:06 -0600

    Git Ignore Fix

--- wp-content/themes/aktion-lab/templates/front-page.php
+++ wp-content/themes/aktion-lab/templates/front-page.php
@@ -1,1 +1,18 @@
-<?php if (function_exists('nivoslider4wp_show')) { nivoslider4wp_show(); } ?>
+
+<nav id="nav-main" role="navigation">
+  <?php wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav')); ?>
+</nav>
+<div id="home-boxes" class="row">
+  <div class="hb-about span3">
+    <h2><a href="">What is it?</a></h2>
+  </div>
+  <div class="hb-find span3">
+    <h2><a href="">Find a run</a></h2>
+  </div>
+  <div class="hb-gallery span3">
+    <h2><a href="">Gallery</a></h2>
+  </div>
+  <div class="hb-press span3">
+    <h2><a href="">Press</a></h2>
+  </div>
+</div><!--row-->  
